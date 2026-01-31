1dd98530a2a85e3cfd44c3fbf27daa67fabd6b7e
Dogpaww <nishadhubert@gmail.com>
2026-01-31 19:16:39 +0530

    added live clock, fixed wrong imagelink issues

--- calendar.js
+++ calendar.js
@@ -8,18 +8,24 @@ function Makecalendar(){
     const months=['January','February','March','April','May','June','July','August','September','October','November','December'];
     const days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
-    document.querySelector("#Year").textContent=year
+    document.querySelector("#Year").textContent=year;
     document.querySelector("#Date").textContent=months[month]+" "+actualdate;
     document.querySelector("#Day").textContent=days[day];
 
     const hour=date.getHours();
     const minute=date.getMinutes();
-    const formattedHour = String(hour).padStart(2, '0'); //so i can get 12:05 and not 12:5
-    const formattedMinute = String(minute).padStart(2, '0');
+    const second=date.getSeconds();
 
-    document.querySelector("#Clock").textContent=formattedHour+":"+formattedMinute;
+    document.querySelector('#Clock').textContent=hour.toString().padStart(2,0)+':'+minute.toString().padStart(2,0)+':'+second.toString().padStart(2,0);
 
-    if(hour>6 && hour<18){
+
+}
+
+function catCheck(){
+
+    const hour=date.getHours();
+
+     if(hour>6 && hour<18){
         document.getElementById("cat").src="./assets/sleepingcat.gif";
         document.querySelector(".Cat").style.right="9px"
         document.querySelector(".Cat").style.top="52px"
@@ -28,10 +34,13 @@ function Makecalendar(){
     else if(hour>18 || hour<6){
         document.getElementById("cat").src="./assets/cat-roll.gif";
     }
-
-
-
-
 }
 
+
 Makecalendar();
+
+setInterval(Makecalendar,100)
+
+catCheck();
+
+setInterval(catCheck(),5*6000);
